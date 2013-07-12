dd5d6b7dd1c241948c764d9700b36b315bf4a6a1
aldrinagana <aldrinagana@gmail.com>
2013-07-12 00:04:12 -0700

    added new test handler

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,10 +1,29 @@
+# Simple request handler
+
+
 import webapp2
 
+form = """
+<form method="post" action="/testform">
+    <input name="q">
+    <input type="submit">
+</form>
+"""
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello Udacity!')
+        #self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(form)
+
+class TestHandler(webapp2.RequestHandler):
+    def post(self):
+        q = self.request.get("q")
+        self.response.write(q)
+
+        #self.response.headers['Content-Type'] = 'text/plain'
+        #self.response.write(self.request)
 
 
-application = webapp2.WSGIApplication([('/', MainPage), ], debug=True)
+application = webapp2.WSGIApplication([('/', MainPage),
+                                       ('/testform', TestHandler)], 
+                                       debug=True)
